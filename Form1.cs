46f166a2b780a0754885a704cb1e0c9bc244dbcd
AidarSalakhov <adr-sal@yandex.ru>
2022-07-27 22:16:27 +0300

    Добавил загрузку городов из файла

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,7 @@ namespace WeatherParserHttpGet
         private void button1_Click(object sender, EventArgs e)
         {
             Program.weatherResponse.DisplayWeather(Program.weatherResponse.GetWeather(), richTextBox1);
+            GetCities();
         }
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
@@ -35,6 +36,27 @@ namespace WeatherParserHttpGet
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        public void GetCities()
+        {
+            try
+            {
+                StreamReader streamReader = new StreamReader("cities.txt");
+                CitiesTreeBuilder tree = new CitiesTreeBuilder();
+                var nodes = tree.GetListNode(streamReader).ToArray();
+                treeView1.Nodes.Clear();
+                foreach (var item in nodes) treeView1.Nodes.Add(item);
+                streamReader.Dispose();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
